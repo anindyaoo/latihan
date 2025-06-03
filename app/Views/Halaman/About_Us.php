c774f95a024a03ed75773fd2ce8b98bff215271c
pemweb1 <aniendyao@gmail.com>
2025-06-03 21:10:46 +0700

    insert data in CI

--- app/Views/Halaman/About_Us.php
+++
@@ -1,5 +0,0 @@
-<?= $this->extend('layout/layout.php') ?>
-
-<?= $this->section('content') ?>
-<h1>Tentang Kami</h1>
-<?= $this->endSection() ?>
--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $title ?? '' ?></title>
+    <?= $this->renderSection('style') ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
@@ -12,7 +10,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Perpustakaan</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -20,13 +18,13 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Detai Buku</a>
+                        <a class="nav-link active" aria-current="page" href="#">Daftar Buku</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Detail Buku</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Tambah Buku</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
@@ -35,6 +33,6 @@
             </div>
         </div>
     </nav>
-</body>
 
-</html>
+    <?= $this->renderSection('content') ?>
+</body>
