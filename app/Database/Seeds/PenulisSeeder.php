2ba106d211382c13279a8d338cfb70c08306d2e6
pemweb1 <aniendyao@gmail.com>
2025-06-16 22:35:54 +0700

    menambahkan seeder

---
+++ app/Database/Seeds/PenulisSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+use Faker\Factory;
+
+class PenulisSeeder extends Seeder
+{
+    public function run()
+    {
+        // $data = [
+        //   'name' => 'Anindya',
+        // 'address' => 'Jl Gusdur',
+        // 'created_at' => Time::now(),
+        //'updated_at' => Time::now()
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+
+            $data = [
+                'name' => $faker->name,
+                'address' => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at' => Time::now()
+            ];
+            $this->db->table('penulis')->insert($data);
+
+        }
+
+        // Simple Queries
+        //   $this->db->query('INSERT INTO penulis (name, address, created_at, updated_at) VALUES (:name:, :address:, :created_at:, :updated_at:)', $data);
+
+        // Using Query Builder
+        //$this->db->table('penulis')->insertBatch($data);
+    }
+}
