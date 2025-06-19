0b8f9c458a65db203bd3ee88911e20b2214a1a68
pemweb1 <aniendyao@gmail.com>
2025-06-19 20:00:22 +0700

    menambahkan fitur upload gambar di form

--- app/Views/books_view/tambah.php
+++ app/Views/books_view/tambah.php
@@ -1,18 +1,16 @@
-<?= $this->section('style') ?>
-<link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-<?= $this->endSection() ?>
-<?= $this->extend('layout/layout'); ?>
+<?= $this->extend('layout/layout') ?>
 
-<?php $this->section('content'); ?>
+<?= $this->section('content') ?>
 <div class="container">
     <div class="row">
         <div class="col-8">
             <h2 class="my-3">Form Tambah Data Buku</h2>
+
             <!-- validasi -->
             <?php if (isset($validation)): ?>
             <?php endif; ?>
 
-            <form action="/simpan" method="post">
+            <form action="<?= base_url('/simpan'); ?>" method="post" enctype="multipart/form-data">
                 <?= csrf_field(); ?>
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
@@ -20,35 +18,57 @@
                         <input type="judul"
                             class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul"
                             name="judul" autofocus value="<?= old('judul'); ?>">
-                    </div>
-                    <div class="invalid-feedback">
-                        <?= $validation->getError('judul'); ?>
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('judul'); ?>
+                        </div>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-10">
-                        <input type="penulis" class="form-control" id="penulis" name="penulis"
-                            value="<?= old('penulis'); ?>">
+                        <input type="penulis"
+                            class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' : ''; ?>"
+                            id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('penulis'); ?>
+                        </div>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="penberit" class="col-sm-2 col-form-label">Penerbit</label>
+                    <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                     <div class="col-sm-10">
-                        <input type="penerbit" class="form-control" id="penberit" name="penerbit">
+                        <input type="penerbit"
+                            class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' : ''; ?>"
+                            id="penerbit" name="penerbit" value="<?= old('penerbit'); ?>">
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('penerbit'); ?>
+                        </div>
                     </div>
                 </div>
+
                 <div class="row mb-3">
                     <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
-                    <div class="col-sm-10">
-                        <input type="sampul" class="form-control" id="sampul" name="sampul">
+                    <div class="col-sm-2">
+                        <img src="img/no-cover.png" class="img-thumbnail img_preview" alt="">
+                    </div>
+
+                    <div class="col-sm-8">
+                        <div class="input group mb-3">
+                            <input type="file"
+                                class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>"
+                                id="sampul" name="sampul" onchange="previewImg()">
+                            <div class="invalid-feedback">
+                                <?= $validation->getError('sampul'); ?>
+                            </div>
+                        </div>
+                        <label for="sampul" class="input-group-text">Upload</label>
                     </div>
                 </div>
 
                 <button type="sumbit" class="btn btn-primary">Tambah Data</button>
-            </form>
 
+            </form>
         </div>
     </div>
 </div>
-<?php $this->endSection(); ?>
+<?= $this->endSection() ?>
