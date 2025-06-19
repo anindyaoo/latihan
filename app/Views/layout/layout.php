fc231c206b7f9790bbfb63ed97f865e75bf06bae
pemweb1 <aniendyao@gmail.com>
2025-06-19 20:00:40 +0700

    menambahkan js

--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -1,10 +1,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? '' ?></title>
-    <?= $this->renderSection('style') ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title><?= $title ?? '' ?></title>
+    <?= $this->renderSection('style') ?>
 </head>
 
 <body>
@@ -35,4 +35,24 @@
     </nav>
 
     <?= $this->renderSection('content') ?>
+
+    <script>
+        function previewImg() {
+
+            const sampul = document.querySelector('#sampul');
+            const sampulLabel = document.querySelector('.input-group-text');
+            const imgPreview = document.querySelector('.img_preview');
+
+            if (sampul.files.length > 0) {
+                sampulLabel.textContent = sampul.files[0].name;
+
+                const fileSampul = new FileReader();
+                fileSampul.readAsDataURL(sampul.files[0]);
+
+                fileSampul.onload = function (e) {
+                    imgPreview.src = e.target.result;
+                }
+            }
+        }
+    </script>
 </body>
